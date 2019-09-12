54ec24ad6dc4e7a597da17c8b3ebcebb4ff86f76
oberstrike <markus.juergens@gmx.de>
2019-09-12 14:13:59 +0200

    Initial commit

---
+++ app/src/main/java/main/com/calendarapp/MainActivity.kt
@@ -0,0 +1,56 @@
+package main.com.calendarapp
+
+import android.content.Intent
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.CalendarView
+
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+import java.util.logging.Logger
+
+class MainActivity : AppCompatActivity() {
+
+    private val logger: Logger = Logger.getLogger("MainLogger")
+
+    override fun onDestroy() {
+        super.onDestroy()
+        logger.info("Destroy")
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+
+        calendarView.setOnDateChangeListener { _, year, month, dayOfMonth ->
+           val newIntent: Intent = Intent()
+        }
+        logger.info("Create")
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
