fd120c2bb3f08f4b713b836971b4ea5608b3b098
Markus Jürgens <Markus.juergens@gmx.de>
2019-10-03 14:41:24 +0200

    Update

--- app/src/main/java/main/com/calendarapp/models/Exercise.kt
+++ app/src/main/java/main/com/calendarapp/models/Exercise.kt
@@ -15,22 +15,40 @@ import org.joda.time.DateTime
 @Entity
 data class Exercise(@Id var id:Long = 0,
                     val name: String,
-                    @Convert(converter = WorkoutSetListConverter::class, dbType = String::class) val workoutSets: MutableList<WorkoutSet> = ArrayList()){
+                    @Convert(converter = WorkoutSetArrayConverter::class, dbType = String::class) var workoutSets: Array<WorkoutSet>){
 
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
 
+        other as Exercise
+
+        if (id != other.id) return false
+        if (name != other.name) return false
+        if (!workoutSets.contentEquals(other.workoutSets)) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = id.hashCode()
+        result = 31 * result + name.hashCode()
+        result = 31 * result + workoutSets.contentHashCode()
+        return result
+    }
 }
 
 data class ExerciseDAO(val id: Long, val name: String, val workoutSets: MutableList<WorkoutSet>)
 
 
-class WorkoutSetListConverter : PropertyConverter<MutableList<WorkoutSet>, String> {
+class WorkoutSetArrayConverter : PropertyConverter<Array<WorkoutSet>, String> {
 
-    override fun convertToDatabaseValue(entityProperty: MutableList<WorkoutSet>?): String {
+    override fun convertToDatabaseValue(entityProperty: Array<WorkoutSet>?): String {
         return GsonBuilder().create().toJson(entityProperty)
     }
 
-    override fun convertToEntityProperty(databaseValue: String?): MutableList<WorkoutSet> {
-        val type = object : TypeToken<List<WorkoutSet>>() {}.type
+    override fun convertToEntityProperty(databaseValue: String?): Array<WorkoutSet> {
+        val type = object : TypeToken<Array<WorkoutSet>>() {}.type
         return GsonBuilder().create().fromJson(databaseValue, type)
     }
 
--- app/src/main/java/main/com/calendarapp/models/WorkoutSet.kt
+++ app/src/main/java/main/com/calendarapp/models/WorkoutSet.kt
@@ -2,6 +2,5 @@ package main.com.calendarapp.models
 
 
 data class WorkoutSet(val repetitions: Long,
-                      val weight:Long,
-                      val name: String)
+                      val weight:Long)
 
